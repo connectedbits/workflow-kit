bbd6f5ec4df16a136189f3e7e9fa35fb64b3de91
Elijah Miller <elijah.miller@gmail.com>
2026-02-09 22:27:08 -0500

    Also make + - / * operations left associative.

--- feel/lib/feel/nodes.rb
+++ feel/lib/feel/nodes.rb
@@ -308,54 +308,6 @@ module FEEL
   end
 
   #
-  # 21. addition = expression , "+" , expression ;
-  #
-  class Addition < Node
-    def eval(context = {})
-      head_val = head.eval(context)
-      tail_val = tail.eval(context)
-      return nil if head_val.nil? || tail_val.nil?
-      head_val + tail_val
-    end
-  end
-
-  #
-  # 22. subtraction = expression , "-" , expression ;
-  #
-  class Subtraction < Node
-    def eval(context = {})
-      head_val = head.eval(context)
-      tail_val = tail.eval(context)
-      return nil if head_val.nil? || tail_val.nil?
-      head_val - tail_val
-    end
-  end
-
-  #
-  # 23. multiplication = expression , "\*" , expression ;
-  #
-  class Multiplication < Node
-    def eval(context = {})
-      head_val = head.eval(context)
-      tail_val = tail.eval(context)
-      return nil if head_val.nil? || tail_val.nil?
-      head_val * tail_val
-    end
-  end
-
-  #
-  # 24. division = expression , "/" , expression ;
-  #
-  class Division < Node
-    def eval(context = {})
-      head_val = head.eval(context)
-      tail_val = tail.eval(context)
-      return nil if head_val.nil? || tail_val.nil?
-      head_val / tail_val
-    end
-  end
-
-  #
   # 25. exponentiation = expression, "\*\*", expression ;
   #
   class Exponentiation < Node
